982144dc9d4f0d74f185400f638791877175fd0c
bookstore <booookstore@icloud.com>
2018-03-04 13:21:28 +0900

    fix : split file

--- kata.hs
+++ kata.hs
@@ -1,35 +1,57 @@
 -- {-# OPTIONS -Wall -Werror #-}
 
-maximum' :: (Ord a) => [a] -> a
-maximum' [] = error "maximum of empty list"
-maximum' [x] = x
-maximum' (x:xs) = max x (maximum' xs)
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
-replicate' :: Int -> a -> [a]
-replicate' n x
-  | n <= 0 = []
-  | otherwise = x : replicate' (n-1) x
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
 
-take' :: Int -> [a] -> [a]
-take' n _
- | n <= 0  = [] -- ガード条件に合致しなければ次のパターンに移る
-take' _ [] = []
-take' n (x:xs) = x : take' (n-1) xs
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe _ = "Not between 1 and 3"
 
-reverse' :: [a] -> [a]
-reverse' [] = []
-reverse' (x:xs) = reverse' xs ++ [x]
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n-1)
 
-repeat' :: a -> [a]
-repeat' x = x : repeat' x
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
-zip' :: [a] -> [b] -> [(a,b)]
-zip' _ [] = []
-zip' [] _ = []
-zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list"
+head' (x:_) = x
 
-elem' :: (Eq a) => a -> [a] -> Bool
-elem' a [] = False
-elem' a (x:xs)
-  | a == x    = True
-  | otherwise = a `elem'` xs
+firstLetter :: String -> String
+firstLetter "" = "Empty string, whoops"
+firstLetter xss@(x:_) = "The first letter of " ++ xss ++ " is " ++ [x]
+
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= fat = "You're fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+  where bmi = weight / height ^ 2
+        (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+  | a <= b    = b
+  | otherwise = a
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+  | a == b    = EQ
+  | a <= b    = LT
+  | otherwise = GT
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+  where (f:_) = firstname
+        (l:_) = lastname
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+  where bmi weight height = weight / height ^ 2
