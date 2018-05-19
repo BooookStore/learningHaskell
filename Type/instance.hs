0c3057d108835ee316b613156fe1eb62c46981bd
bookstore <booookstore@icloud.com>
2018-05-19 12:02:13 +0900

    delete sketch code

--- Type/instance.hs
+++ Type/instance.hs
@@ -12,11 +12,3 @@ instance Next TraficLight where
     ok Red = False
     ok Yellow = False
     ok Blue = True
-
-data Point a = Point a a
-
-class Move a where
-    move :: a -> Float -> a
-
-instance (Float a) => Move (Float a) where
-    move (Point x y) a = Point (x+a) (y+a)
