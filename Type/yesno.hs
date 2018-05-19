0cda4bf46af3b68c1751bcb7bdc69ef4825e5f97
bookstore <booookstore@icloud.com>
2018-05-19 16:17:17 +0900

    implement yesno class and instance

---
+++ Type/yesno.hs
@@ -0,0 +1,17 @@
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id -- id は引数をそのまま返す標準ライブラリ関数
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
