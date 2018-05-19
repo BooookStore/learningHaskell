ec0a625224ce369215967016ef848f35d75305ce
bookstore <booookstore@icloud.com>
2018-05-19 16:43:46 +0900

    implementing Functor' ...

---
+++ Type/functor.hs
@@ -0,0 +1,20 @@
+-- Functor は あるデータ型が持っている値を別の値へ変える
+-- f は一つの型引数を取る型コンストラクタ
+class Functor' f where
+    fmap' :: (a -> b) -> f a -> f b
+
+-- リストに対するFunctor
+-- [] はリストの型コンストラクタ
+instance Functor' [] where
+    fmap' = map
+
+-- Maybeに対するFunctor
+instance Functor' Maybe where
+    fmap' f (Just x) = Just (f x)
+    fmap' f Nothing = Nothing
+
+data Box a = EmptyBox | Box a deriving (Show)
+
+instance Functor' Box where
+    fmap' f (Box a) = Box (f a)
+    fmap' f EmptyBox = EmptyBox
