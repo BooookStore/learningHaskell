2441deff4845d39242974b547dca79da92a6ba60
Honya <booookstore@icloud.com>
2018-05-26 21:08:39 +0900

    create interact example

---
+++ FileIO/palindrome.hs
@@ -0,0 +1,14 @@
+-- 回文かを判定する
+
+import Data.Char
+
+main = interact respondPalindromes
+
+respondPalindromes :: String -> String
+respondPalindromes =
+    unlines .
+    map (\xs -> if isPal xs then "palindrome" else "not a palindrome") .
+    lines
+
+isPal :: String -> Bool
+isPal xs = xs == reverse xs
