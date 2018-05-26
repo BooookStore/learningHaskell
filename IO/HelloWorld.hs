a5c7e750e15912561dd86d7bde67bd09ac09fb4a
bookstore <booookstore@icloud.com>
2018-05-26 20:33:17 +0900

    update IO

--- IO/HelloWorld.hs
+++ IO/HelloWorld.hs
@@ -1,6 +1,21 @@
+import System.IO
+import Control.Monad
+import Data.Char
+
 main = do
-    let x = "A"
-    return ()
+    colors <- forM [1,2,3,4] $ \a -> do
+        putStrLn $ "Which color do you associate with the number " ++ show a ++ "?"
+        getLine
+    putStrLn "The colors that you associate with 1,2,3 and 4 are : "
+    mapM putStr colors
 
 reverseWords :: String -> String
 reverseWords = unwords . map reverse . words
+
+data Box a = Box a deriving (Show)
+
+prompt :: String -> IO String
+prompt text = do
+    putStr text
+    hFlush stdout
+    getLine
